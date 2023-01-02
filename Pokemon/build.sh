73c495115435dc263b9f427b998b99f8b0fc5f31
Alexander Hebel <a.hebelsun@gmail.com>
2023-01-02 22:56:15 +0100

    add cmake package to install in build.sh

--- Pokemon/build.sh
+++ Pokemon/build.sh
@@ -4,6 +4,7 @@ sudo apt install libsdl2-dev
 sudo apt-get install libsdl2-mixer-dev
 sudo apt-get install libsdl2-image-dev
 sudo apt-get install libsdl2-ttf-dev
+sudo apt-get install build-essential cmake
 
 mkdir build
 cd build
